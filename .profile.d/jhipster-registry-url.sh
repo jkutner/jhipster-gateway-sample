1ca35c3e3d8387cbf96fbfab06d68075f423baa5
Joe Kutner <jpkutner@gmail.com>
2016-06-25 00:00:22 -0500

    Fixed registry url

--- .profile.d/jhipster-registry-url.sh
+++ .profile.d/jhipster-registry-url.sh
@@ -1,1 +1,1 @@
-export JHIPSTER_REGISTRY_URL=${JHIPSTER_REGISTRY_URL:-"https://admin:${JHIPSTER_PASSWORD}@spring-one-registry.herokuapp.com/eureka/"}
+export JHIPSTER_REGISTRY_URL=${JHIPSTER_REGISTRY_URL:-"https://admin:${JHIPSTER_PASSWORD}@spring-one-registry.herokuapp.com"}
