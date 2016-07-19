01b5270c9e3468d7b197ead63b7b7d9e8006cfa0
Joe Kutner <jpkutner@gmail.com>
2016-07-19 10:21:47 -0500

    Cleaned up error server

--- .profile.d/jhipster-registry-url.sh
+++ .profile.d/jhipster-registry-url.sh
@@ -2,11 +2,7 @@ export JHIPSTER_REGISTRY_URL=${JHIPSTER_REGISTRY_URL:-"https://admin:${JHIPSTER_
 
 status=$(curl --retry 3 --silent --head -w %{http_code} -L ${JHIPSTER_REGISTRY_URL} -o /dev/null)
 if [ "$status" != "200" ]; then
-    echo "
-Uh oh! The JHipster Registry is not available.
-You can learn how to create your own at:
-https://jhipster.github.io/heroku/
-"
-    msg="Uh oh! The JHipster Registry is not available.<p>You can learn how to create your own at:<p>https://jhipster.github.io/heroku/"
-    ruby -rwebrick -e"s=WEBrick::HTTPServer.new(:Port => $PORT, :DocumentRoot => Dir.pwd); s.mount_proc('/'){|q,r| r.body='${msg}'}; s.start"
+    msg="Uh oh! The JHipster Registry is not available. You can learn how to create your own at: https://jhipster.github.io/heroku/"
+    echo "$msg"
+    ruby -rwebrick -e"s=WEBrick::HTTPServer.new(:BindAddress => \"0.0.0.0\", :Port => $PORT, :DocumentRoot => Dir.pwd); s.mount_proc('/'){|q,r| r.body='${msg}'}; s.start"
 fi
